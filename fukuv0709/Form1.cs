16d3a7c28dafaae3317308349c9000d983e855e3
kai <kai03210607@gmail.com>
2020-07-09 09:32:28 +0900

    未完成

--- fukuv0709/Form1.cs
+++ fukuv0709/Form1.cs
@@ -16,5 +16,11 @@ namespace fukuv0709
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
     }
 }
