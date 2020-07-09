234abba0305d68d181e956122a4305f6dae36f33
kai <kai03210607@gmail.com>
2020-07-09 09:42:29 +0900

    未完成

--- fukuv0709/Form1.Designer.cs
+++ fukuv0709/Form1.Designer.cs
@@ -52,6 +52,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -61,6 +62,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
--- fukuv0709/Form1.cs
+++ fukuv0709/Form1.cs
@@ -20,7 +20,17 @@ namespace fukuv0709
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + -10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + -10;
+
         }
     }
 }
